7cafecd7918d1e801362556967b4e8e25111fe65
noamhertz <Se242010>
2019-11-16 18:05:55 +0200

    movieDB creation 15 features + revenue

--- loading_script.py
+++ loading_script.py
@@ -6,6 +6,7 @@ TRAIN_CSV_PATH = './datasets/train.csv'
 TEST_CSV_PATH = './datasets/test.csv'
 CAST_NAME_REGEX = "(name\\':\ \\')([A-Z].+)(\\', \\'order)"
 CAST_MAX_PRIO = 3
+PROD_MAX_PRIO = 3
 
 #filter thresholds
 CAST_THRESH = 2
@@ -195,15 +196,12 @@ def filter_vocabs(vocab, thresh):
         if len(vocab[item]) >= thresh:
             filtered_vocab[item] = vocab[item]
     return filtered_vocab
-#############################
+
 def count_vocab(vocab):
     appearance_vocab = {}
     for item in vocab:
         appearance_vocab[item] = len(vocab[item])
     return sorted(appearance_vocab.items(), key=lambda x:x[1])
-
-
-
 
 def geners_vocab_list(dataset):
     genres_vocab = []
@@ -213,21 +211,132 @@ def geners_vocab_list(dataset):
         genres_vocab.append([])
         for genre in movie:
             genres_vocab[count].append(genre["name"])
-        count +=1
+        count += 1
     return genres_vocab
 
-def revenue_list(dataset):
-    revenues = []
-    rev_col = dataset["revenue"]
-    for movie in rev_col:
-        revenues.append(movie)
-    return revenues
 
+
+def get_cast_list(cast,movie_id):
+    cast_list = []
+    cast_counter = 0
+    for cast_member in cast[movie_id-1]:
+        if cast_counter >= CAST_MAX_PRIO:
+            break
+        cast_list.append(cast_member["name"])
+        cast_counter += 1
+    return cast_list
+
+def get_director_list(directors, movie_id):
+    director_list = []
+    for director in directors[movie_id-1]:
+        if director["job"] == "Director":
+            director_list.append(director["name"])
+            break
+    return director_list
+
+def get_prod_comps_list(prod_comps, movie_id):
+    prod_comp_list = []
+    prod_comp_counter = 0
+    for prod_comp in prod_comps[movie_id-1]:
+        if prod_comp_counter >= PROD_MAX_PRIO:
+            break
+        prod_comp_list.append(prod_comp["name"])
+        prod_comp_counter += 1
+    return prod_comp_list
+
+def get_languages_list(languages, movie_id):
+    languages_list = []
+    for language in languages[movie_id-1]:
+        languages_list.append(language["iso_639_1"])
+    return languages_list
+
+def get_name_list(items, movie_id):
+    items_list = []
+    for item in items[movie_id - 1]:
+        items_list.append(item["name"])
+    return items_list
+
+def get_release_year_list(release_dates, movie_id):
+    date = dt.strptime(release_dates[movie_id-1], "%m/%d/%y")
+    if date.year > dt.now().year:
+        year = date.year - 100
+    else:
+        year = date.year
+    return [year]
+
+def get_release_month_list(release_dates, movie_id):
+    date = dt.strptime(release_dates[movie_id-1], "%m/%d/%y")
+    return [date.month]
+
+def get_weekday_list(release_dates, movie_id):
+    release_date = dt.strptime(release_dates[movie_id-1], "%m/%d/%y")
+    if release_date.year > dt.now().year:
+        year = release_date.year - 100
+    else:
+        year = release_date.year
+    year  = str(year)
+    month = str(release_date.month)
+    day   = str(release_date.day)
+    date_str = year + month + day
+    s_datetime = dt.strptime(date_str, '%Y%m%d')
+    weekday = s_datetime.weekday()
+    return [weekday]
+
+def get_is_collection_list(collections, movie_id):
+    if not collections[movie_id-1]:
+        return [0]
+    else:
+        return [1]
+
+def get_num_words_list(titles, movie_id):
+    title = titles[movie_id-1]
+    return [len(title.split())]
+
+def get_number_list(items,movie_id):
+    item = items[movie_id-1]
+    return [item]
+
+def createDB(dataset):
+    movieDB = {}
+    ids = dataset["id"]
+    genres      = dictioning_column(dataset["genres"])
+    cast        = dictioning_column(dataset["cast"])
+    directors   = dictioning_column(dataset["crew"])
+    keywords    = dictioning_column(dataset["Keywords"])
+    prod_comps  = dictioning_column(dataset["production_companies"])
+    languages   = dictioning_column(dataset["spoken_languages"])
+    countries   = dictioning_column(dataset["production_countries"])
+    collections = dictioning_column(dataset["belongs_to_collection"])
+    titles      = dataset["original_title"]
+    date        = dataset["release_date"]
+    budget      = dataset["budget"]
+    runtime     = dataset["runtime"]
+    popularity  = dataset["popularity"]
+    revenue     = dataset["revenue"]
+    for movie_id in ids:
+        movieDB[movie_id] = {}
+        movieDB[movie_id]["genres"]         = get_name_list(genres, movie_id)
+        movieDB[movie_id]["cast"]           = get_cast_list(cast, movie_id)
+        movieDB[movie_id]["director"]       = get_director_list(directors, movie_id)
+        movieDB[movie_id]["keywords"]       = get_name_list(keywords, movie_id)
+        movieDB[movie_id]["prod_companies"] = get_prod_comps_list(prod_comps, movie_id)
+        movieDB[movie_id]["languages"]      = get_languages_list(languages, movie_id)
+        movieDB[movie_id]["countries"]      = get_name_list(countries, movie_id)
+        movieDB[movie_id]["is_collection"]  = get_is_collection_list(collections, movie_id)
+        movieDB[movie_id]["num_words"]      = get_num_words_list(titles, movie_id)
+        movieDB[movie_id]["release_year"]   = get_release_year_list(date, movie_id)
+        movieDB[movie_id]["release_month"]  = get_release_month_list(date, movie_id)
+        movieDB[movie_id]["weekday"]        = get_weekday_list(date, movie_id)
+        movieDB[movie_id]["budget"]         = get_number_list(budget, movie_id)
+        movieDB[movie_id]["runtime"]        = get_number_list(runtime, movie_id)
+        movieDB[movie_id]["popularity"]     = get_number_list(popularity, movie_id)
+        movieDB[movie_id]["revenue"]        = get_number_list(revenue, movie_id)
+    return movieDB
 
 def test(dataset):
     # YEAR_VOCAB               = init_date_vocab(dataset, "year")
     # MONTH_VOCAB              = init_date_vocab(dataset, "month")
-    GENRES_VOCAB             = init_genres_vocab(dataset)
+    # GENRES_VOCAB             = init_genres_vocab(dataset)
     # CAST_VOCAB               = init_cast_vocab(dataset)
     # DIRECTOR_VOCAB           = init_crew_vocab(dataset, ['Director'])
     # KEYWORDS_VOCAB           = init_keyword_vocab(dataset)
@@ -237,17 +346,14 @@ def test(dataset):
     # LANGUAGE_COUNT           = count_vocab(LANGUAGE_VOCAB)
     # COUNTRY_VOCAB            = init_country_vocab(dataset)
     # COUNTRY_COUNT            = count_vocab(COUNTRY_VOCAB)
-
-
-
+    DB = createDB(dataset)
     #geners_vocab_list(dataset)
-    #revnues = revnue_list(dataset)
     print("test ended")
 
 def main():
     train = get_movies_db(TRAIN_CSV_PATH)
-    test = get_movies_db(TEST_CSV_PATH)
-    test(dataset)
+    # testy = get_movies_db(TEST_CSV_PATH)
+    test(train)
     print("end")
 
 if __name__ == "__main__":
